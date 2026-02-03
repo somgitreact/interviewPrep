02ba701bd331c6dc65f8694810e28eab219941fb
somgitreact <graphicsom@gmail.com>
2026-02-03 16:59:52 +0530

    fix countrydetails 7

--- src/api/Allapi.js
+++ src/api/Allapi.js
@@ -13,14 +13,25 @@ export const Apiresult = async (apiendpoint)=>{
     
 }
 
-export const allPurpousData = async (apiendpoint)=>{
-    try {
-     const res = await axios.get(apiendpoint)
-       if (!res.ok) throw new Error("Network Error");
-    return res.data   
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    return [];   
-    }
+// export const allPurpousData = async (apiendpoint)=>{
+//     try {
+//      const res = await axios.get(apiendpoint)
+//        if (!res.ok) throw new Error("Network Error");
+//     return res.data   
+//     } catch (error) {
+//         console.error("Error fetching data:", error);
+//     return [];   
+//     }
     
-}
+// }
+
+export const allPurpousData = async (url) => {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) throw new Error("Network Error");
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
