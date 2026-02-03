dfc94f15864360358e4184f723e9ad45fe5e21a3
somgitreact <graphicsom@gmail.com>
2026-02-03 19:50:31 +0530

    fix assagement

--- src/pages/Displayname.jsx
+++ src/pages/Displayname.jsx
@@ -1,27 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Displayname = () => {
-    const [input, setInput] = useState({});
-    const [text, setText] = useState('')
-    const inpChang = (e)=>{
-        const {name, value} = e.target;
-        setInput((prev)=>({...prev, [name]:value }))
+  const [input, setInput] = useState({
+    fnam: "",
+    lnam: ""
+  });
+  const [text, setText] = useState("");
+
+  const inpChang = (e) => {
+    const { name, value } = e.target;
+    setInput((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (input.fnam && input.lnam) {
+      setText(`${input.fnam} ${input.lnam}`);
+    } else {
+      setText("");
     }
-    const submitHandler = ()=>{
-     setText(input.fnam + " " + input.lnam)
-    }
+  };
+
   return (
     <div>
-      <div className=''>
-        First Name: <input type='text' name='fnam' value={input.fnam} onChange={inpChang} />
-      </div>
-      <div className=''>
-        Last Name: <input type='text' name='lnam' value={input.lnam} onChange={inpChang} />
-      </div>
-      <button onClick={submitHandler}>Submit</button>
-      {text && <div className='fullname'>Full Name:  {text}</div>}
-    </div>
-  )
-}
+      <h2>Full Name Display</h2>
 
-export default Displayname
+      <form onSubmit={submitHandler}>
+        <div>
+          First Name:
+          <input
+            type="text"
+            name="fnam"
+            value={input.fnam}
+            onChange={inpChang}
+          />
+        </div>
+
+        <div>
+          Last Name:
+          <input
+            type="text"
+            name="lnam"
+            value={input.lnam}
+            onChange={inpChang}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      {text && <div className="fullname">Full Name: {text}</div>}
+    </div>
+  );
+};
+
+export default Displayname;
