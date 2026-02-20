8c85827ef324375fa21f9502bab6448ba9ffb0ad
somgitreact <graphicsom@gmail.com>
2026-02-20 22:59:27 +0530

    upload employee fix 4.

--- src/api/Allapi.js
+++ src/api/Allapi.js
@@ -5,13 +5,14 @@ export const Apiresult = async (apilink)=>{
     try {
     // const res = await axios.get(` https://countries-search-data-prod-812920491762.asia-south1.run.app`)
     const res = await axios.get(apilink)
-    console.log(res );    
-       if (!res.ok) throw new Error("Network Error");
+    //console.log(res );    
+      // if (!res.ok) throw new Error("Network Error");
       //if (res.status !== 200) throw new Error("Network Error");
     return res.data   
     } catch (error) {
+       
         console.error("Error fetching data:", error); 
-        alert(error.message)
+         alert("Failed to fetch data");
     return [];   
     }
     
